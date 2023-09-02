dcaa074e9a47e86a3261228cb1975bb663832caf
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-09-02 17:03:02 +0300

    retrun int_to_str function

--- src/utils.f90
+++ src/utils.f90
@@ -1,7 +1,7 @@
 module Utils
     implicit none
     private
-    public :: loadToken, int_to_str, startsWith, replaceStr
+    public :: loadToken, startsWith, replaceStr, int_to_str
     
     contains
     function loadToken() result(token)
@@ -14,44 +14,6 @@ module Utils
         read(unit, '(A)') token
         close(unit) 
     end function
-
-    ! https://github.com/rajkumardongre/github-org-analyzer/blob/master/src/utils.f90
-    function int_to_str(int) result(str)
-        integer, intent(in) :: int
-        character(:), allocatable :: str
-        integer :: j, temp, rem
-        integer, allocatable :: nums(:)
-
-        ! Initialize variables
-        temp = int
-        str = ''
-
-        ! Convert the integer to its string representation
-        do while (temp > 9)
-            rem = mod(temp, 10)
-            temp = temp / 10
-            if (allocated(nums)) then
-                nums = [rem, nums]
-            else
-                nums = [rem]
-            end if
-        end do
-
-        ! Add the last digit to the string
-        if (allocated(nums)) then
-            nums = [temp, nums]
-        else
-            nums = [temp]
-        end if
-
-        ! Convert the individual digits to characters and concatenate them
-        do j = 1, size(nums)
-            str = str // achar(nums(j) + 48)
-        end do
-
-        ! Deallocate the temporary array
-        deallocate(nums)
-    end function int_to_str
 
     ! https://github.com/ecasglez/FortranUtilities/blob/master/src/Strings_M.F90#L527
     function startsWith(str, substr) RESULT(res)
@@ -104,4 +66,41 @@ module Utils
             cycle
         end do
     end function replaceStr
+
+    function int_to_str(int) result(str)
+        integer, intent(in) :: int
+        character(:), allocatable :: str
+        integer :: j, temp, rem
+        integer, allocatable :: nums(:)
+
+        ! Initialize variables
+        temp = int
+        str = ''
+
+        ! Convert the integer to its string representation
+        do while (temp > 9)
+            rem = mod(temp, 10)
+            temp = temp / 10
+            if (allocated(nums)) then
+                nums = [rem, nums]
+            else
+                nums = [rem]
+            end if
+        end do
+
+        ! Add the last digit to the string
+        if (allocated(nums)) then
+            nums = [temp, nums]
+        else
+            nums = [temp]
+        end if
+
+        ! Convert the individual digits to characters and concatenate them
+        do j = 1, size(nums)
+            str = str // achar(nums(j) + 48)
+        end do
+
+        ! Deallocate the temporary array
+        deallocate(nums)
+    end function int_to_str
 end module
